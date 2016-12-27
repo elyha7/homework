31cb440fe05b04959b7b656c317e5a8e0452ed58
Artem Rebrikov <artemrebrikov@gmail.com>
2016-12-27 12:32:43 +0300

    final version of FF alg

--- FFalg.py
+++ FFalg.py
@@ -1,3 +1,14 @@
+import itertools as it
+import sys
+import pickle
+def Magic4to3(FourToThree,request):
+        for i in it.permutations(request):
+            if str(i) in FourToThree.keys():
+                Result=FourToThree[str(i)]
+                return Result
+def Magic3to4(ThreeToFour,request):
+    return ThreeToFour[str(request)]
+
 class Edge(object):
     def __init__(self, u, v, w):
         self.source = u
@@ -5,7 +16,7 @@ class Edge(object):
         self.capacity = w
 
     def __repr__(self):
-        return "%s->%s:%s" % (self.source, self.target, self.capacity)
+        return "%s~%s" % (self.source, self.target)
 
 
 class FlowNetwork(object):
@@ -14,7 +25,10 @@ class FlowNetwork(object):
         self.flow = {}
 
     def AddVertex(self, vertex):
+        #if self.adj.get(vertex)==None:
         self.adj[vertex] = []
+        #else:
+         #   return 5
 
     def GetEdges(self, v):
         return self.adj[v]
@@ -33,43 +47,84 @@ class FlowNetwork(object):
         self.flow[redge] = 0
 
     def FindPath(self, source, target, path):
-        result=None
         if source == target:
             return path
         for edge in self.GetEdges(source):
             residual = edge.capacity - self.flow[edge]
             if residual > 0 and not (edge, residual) in path:
                 result = self.FindPath(edge.target, target, path + [(edge, residual)])
-            if result != None:
-                return result
+                if result != None:
+                    return result
 
     def MaxFlow(self, source, target):
         path = self.FindPath(source, target, [])
-        print ('path after enter MaxFlow: %s' % path)
-        for key in self.flow:
-              print ('%s:%s' % (key,self.flow[key]))
-        print ('-' * 20)
+        #print 'path after enter MaxFlow: %s' % path
+        #for key in self.flow:
+        #      print '%s:%s' % (key,self.flow[key])
+        #print '-' * 20
         while path != None:
             flow = min(res for edge, res in path)
             for edge, res in path:
                 self.flow[edge] += flow
                 self.flow[edge.redge] -= flow
-            for key in self.flow:
-                print ('%s:%s' % (key,self.flow[key]))
+            #for key in self.flow:
+            #    print '%s:%s' % (key,self.flow[key])
             path = self.FindPath(source, target, [])
-            print ('path inside of while loop: %s' % path)
-            for key in self.flow:
-                print ('%s:%s' % (key,self.flow[key]))
+            #print 'path inside of while loop: %s' % path
+            #for key in self.flow:
+            #    print '%s:%s' % (key,self.flow[key])
         return sum(self.flow[edge] for edge in self.GetEdges(source))
 
-g = FlowNetwork()
-map(g.AddVertex, ['s', 'o', 'p', 'q', 'r', 't'])
-g.AddEdge('s', 'o', 5)
-g.AddEdge('s', 'p', 3)
-g.AddEdge('o', 'p', 2)
-g.AddEdge('o', 'q', 3)
-g.AddEdge('p', 'r', 4)
-g.AddEdge('r', 't', 3)
-g.AddEdge('q', 'r', 4)
-g.AddEdge('q', 't', 2)
-print (g.MaxFlow('s', 't'))
+
+if __name__ == "__main__":
+
+    gr = FlowNetwork()
+    gr.AddVertex('root')
+    gr.AddVertex('end')
+    if len(sys.argv)>1:
+        args=[]
+        ThreeToFour={}
+        FourToThree={}
+        ThreeToFour = pickle.load( open( "save3t4.p", "rb" ) )
+        FourToThree = pickle.load( open( "save4t3.p", "rb" ) )
+        for i in sys.argv[1:]:
+            args.append(i)
+        #print args
+        req=()
+        if len (args) == 4:
+            for i in args:
+                req=req+(int(i),)
+            print req
+            a= Magic4to3(FourToThree,req)
+            print a
+            sys.exit(0)
+        if len (args) == 3:
+            for i in args:
+                req=req+(int(i),)
+            print req
+            a= Magic3to4(ThreeToFour,req)
+            print a
+            sys.exit(0)
+
+
+
+    for i in it.combinations(range(27),4):
+        gr.AddVertex(i)
+        gr.AddEdge('root',i,1)
+        for t in it.combinations(i,3):
+            for j in it.permutations(t):
+                gr.AddVertex(j)
+                gr.AddEdge(i,j,1)
+                gr.AddEdge(j,'end',1)
+    print gr.MaxFlow('root','end')
+
+    ThreeToFour={}
+    FourToThree={}
+    for i in gr.flow.keys():
+        if gr.flow[i]==1:
+            k=str(i)
+            four,three=k.split('~')
+            FourToThree[four]=three
+            ThreeToFour[three]=four
+    pickle.dump( FourToThree, open( "save4t3.p", "wb" ) )
+    pickle.dump( ThreeToFour, open( "save3t4.p", "wb" ) )
